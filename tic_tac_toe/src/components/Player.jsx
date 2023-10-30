764cc9c770ab95fc631c15f7a93eb688e47dd8eb
Prashant-3103 <prashantkumarchoudhary.211mn035@nitk.edu.in>
2023-10-30 22:22:57 +0530

    second_commit

--- tic_tac_toe/src/components/Player.jsx
+++ tic_tac_toe/src/components/Player.jsx
@@ -1,21 +1,31 @@
 import { useState } from "react"
 
-export default function Player({name, symbol})
+export default function Player({name, symbol, isActive, onChangeName})
 {
+    const [playerName, setPlayerName] =  useState(name)
 const [isEditing, setIsEditing]= useState(false)
-const handleEditing = ()=>{
-    setIsEditing(!isEditing)
+const handleEditing = (e)=>{
+    setIsEditing((editing)=> !isEditing)
+   if(isEditing)
+   {
+    onChangeName(symbol, playerName)
+   }
+}
+
+const handleChange = (e) =>{
+    setPlayerName(e.target.value)
+
 }
 
     return (
-        <li>
+        <li className={isActive ? 'active' : undefined}>
   <span className="player">
-{!isEditing ?   <span className="player-name">{name}</span> : (
-    <input type="text" required />
+{!isEditing ?   <span className="player-name">{playerName}</span> : (
+    <input type="text" required value={playerName} onChange={handleChange} />
 )}
   <span className="player-symbol">{symbol}</span>
   </span>
-  <button onClick={handleEditing} >Edit</button>
+  <button onClick={handleEditing} >{!isEditing ? (<h3>Edit</h3>) : (<h3>Save</h3>)}</button>
 </li>
     )
 }
