bfc7646c8736ec68ba3142723389a66f2aca2033
Prashant-3103 <prashantkumarchoudhary.211mn035@nitk.edu.in>
2023-11-02 15:01:44 +0530

    third_commit

--- prop_drilling_shopping_cart_project/src/App.jsx
+++ prop_drilling_shopping_cart_project/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header.jsx';
 import Shop from './components/Shop.jsx';
 import { DUMMY_PRODUCTS } from './dummy-products.js';
 import Product from './components/Product.jsx'
+import { CartContext } from './store/shopping-cart-context.jsx';
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
     items: [],
@@ -65,20 +66,26 @@ function App() {
     });
   }
 
+  const ctxValue = {
+    items: shoppingCart.items,
+    addItemToCart: handleAddItemToCart,
+    updatedItemQuantity: handleUpdateCartItemQuantity
+  }
+
   return (
-    <>
+    <CartContext.Provider value={ctxValue}>
+
       <Header
-        cart={shoppingCart}
-        onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
+
       />
       <Shop onAddItemToCart={handleAddItemToCart} >
       {DUMMY_PRODUCTS.map((product) => (
           <li key={product.id}>
-            <Product {...product} onAddToCart={handleAddItemToCart} />
+            <Product {...product}  />
           </li>
         ))}
       </Shop>
-    </>
+    </CartContext.Provider>
   );
 }
 
